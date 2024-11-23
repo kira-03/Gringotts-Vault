786dd8a52ab6a16cc5aa041fc01ce173d4331357
Guru <guruprasathmaheswaran.07@gmail.com>
2024-11-24 01:22:33 +0530

    initial commits

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Poppins } from 'next/font/google'
+
+import "./globals.css";
+
+const poppins = Poppins({
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    variable: '--font-poppins',
+})
+
+export const metadata: Metadata = {
+  title: "Gringotts Vault",
+  description: "A secure, magical space to store your files, just like treasures hidden in Gringotts' legendary vaults.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} font-poppins antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
